31edcf32429e50f42b9db1d22f2ac92f14a2f065
Renato Athaydes <renato@athaydes.com>
2020-10-22 22:06:40 +0200

    Improve VerifyJava script

--- __tests__/VerifyJava.java
+++ __tests__/VerifyJava.java
@@ -1,20 +1,37 @@
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class VerifyJava {
     public static void main(String[] args) {
-        if (args.length != 4) throw new IllegalArgumentException("Expected 3 arguments: " + args.length);
+        if (args.length != 5) {
+            throw new IllegalArgumentException("Expected 5 arguments, given " + args.length);
+        }
+        
         String expectedJavaVersion = args[0];
         String expectedVendor = args[1];
         String expectedJavaHome = args[2];
         String javaVersionOutput = args[3];
-        
+        String expectedJavaVersionOutput = args[4];
+
         assertEqual(System.getProperty("java.version"), expectedJavaVersion, "Unexpected version");
         assertEqual(System.getProperty("java.vendor"), expectedVendor, "Unexpected vendor");
-        assertEqual(System.getProperty("java.home"), expectedJavaHome, "Unexpected java.home");
-        assertEqual(expectedJavaVersion, javaVersionOutput, "Unexpected output version");
+        assertEqual(System.getenv("JAVA_HOME"), expectedJavaHome, "Unexpected JAVA_HOME");
+        assertEqual(javaVersionOutput, expectedJavaVersionOutput, "Unexpected output version");
+        
+        assertWithinJavaHome(System.getenv("JAVA_HOME"), System.getProperty("java.home"));
     }
 
     private static void assertEqual(String actual, String expected, String message) {
         if (!actual.equals(expected)) {
             error(message + " (expected = '" + expected + "', actual = '" + actual + "')");
+        }
+    }
+
+    private static void assertWithinJavaHome(String javaHomeEnv, String javaHomeProp) {
+        Path env = Paths.get(javaHomeEnv);
+        Path prop = Paths.get(javaHomeProp);
+        if (!prop.startsWith(env)) {
+            error("Java process java.home (" + prop + ") is not under JAVA_HOME (" + env + ")");
         }
     }
 
